83bd74c074e563fa3c56362284ec95f803330bcc
chuyendev <chuyendev@gmail.com>
2024-01-09 10:57:26 +0700

    update filter team sale

--- mbw_dms/mbw_dms/doctype/dms_router/dms_router.js
+++ mbw_dms/mbw_dms/doctype/dms_router/dms_router.js
@@ -5,4 +5,13 @@ frappe.ui.form.on('DMS Router', {
 	// refresh: function(frm) {
 
 	// }
+	onload: function(frm) {
+		frm.set_query('team_sale',() =>{ 
+			return {
+				filters: {
+					is_group:1
+				}
+			}
+		})
+	}
 });
