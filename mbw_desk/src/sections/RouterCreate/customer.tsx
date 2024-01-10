01dd74f098aefa810668008abd10daf7221b404b
chuyendev <chuyendev@gmail.com>
2024-01-10 14:30:40 +0700

    styled component

--- mbw_desk/src/sections/RouterCreate/customer.tsx
+++ mbw_desk/src/sections/RouterCreate/customer.tsx
@@ -1,31 +1,48 @@
 import React from "react";
 import RowCustom from "./styled";
-import { Button, Col, Input, Row, Select } from "antd";
+import { Button, Col, Input, Radio, Row, Select } from "antd";
 import { FormItemCustom } from "../../components/form-item";
 import { addCustomerOption } from "./data";
-
+import { List, ThunderIcon } from "../../icons";
+import { SearchOutlined } from "@ant-design/icons";
+import { Map } from "../../icons/map";
 export default function Customer() {
+  
   return (
     <>
       <RowCustom className={"justify-between"}>
         <Col>
           <Row gutter={8}>
-            <Col span={9}>
-              <FormItemCustom><Select defaultValue={'add'} options={addCustomerOption}/></FormItemCustom>
+            <Col span={9} className="text-[#1677ff]">
+              <FormItemCustom>
+                <Select defaultValue={"add"} options={addCustomerOption} />
+              </FormItemCustom>
             </Col>
             <Col span={7}>
-              <Button className="w-full">
-                Tối ưu tuyến 
+              <Button
+                className="w-full flex items-center text-[#1677ff] "
+                icon={<ThunderIcon />}
+              >
+                Tối ưu tuyến
               </Button>
             </Col>
             <Col span={8}>
               <FormItemCustom>
-                <Select showSearch placeholder="Tìm kiếm khách hàng"/>
+                <Input
+                  placeholder="Tìm kiếm khách hàng"
+                  prefix={<SearchOutlined />}
+                />
               </FormItemCustom>
             </Col>
           </Row>
         </Col>
-        <Col> Col right</Col>
+        <Col>
+          {" "}
+          <Radio.Group defaultValue="list" >
+            <Radio.Button value="list"><List size={28}/></Radio.Button>
+            <Radio.Button value="map"><Map size={28}/></Radio.Button>
+          </Radio.Group>
+        </Col>
       </RowCustom>
     </>
   );
