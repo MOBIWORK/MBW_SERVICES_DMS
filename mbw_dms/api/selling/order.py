e05e8f7ad46c42ba7d52e312cb969b0cbf6a78cb
chuyendev <chuyendev@gmail.com>
2024-01-17 14:59:03 +0700

    update taxes

--- mbw_dms/api/selling/order.py
+++ mbw_dms/api/selling/order.py
@@ -69,14 +69,13 @@ def get_sale_order(**data):
             Customer = frappe.qb.DocType("Customer")
             SalesOrderItem = frappe.qb.DocType("Sales Order Item")
             SalesOrderTaxes = frappe.qb.DocType("Sales Taxes and Charges")
-            field_detail_sales = ['tax_amount','rate','account_head','charge_type','total','grand_total','customer','customer_name','address_display',"delivery_date",'set_warehouse','taxes_and_charges','total_taxes_and_charges','apply_discount_on','additional_discount_percentage','discount_amount','contact_person','rounded_total']
+            field_detail_sales = ['total','grand_total','customer','customer_name','address_display',"delivery_date",'set_warehouse','taxes_and_charges','total_taxes_and_charges','apply_discount_on','additional_discount_percentage','discount_amount','contact_person','rounded_total']
             # field_detail_taxe  = ['tax_amount','rate','account_head','charge_type']
             field_detail_items = ['item_name','item_code','qty',"uom",'amount','discount_amount','discount_percentage']
             detail = (frappe.qb.from_(SalesOrder)
                     .inner_join(SalesOrderItem)
                     .on(SalesOrder.name == SalesOrderItem.parent)
-                    .inner_join(SalesOrderTaxes)
-                    .on(SalesOrder.name == SalesOrderTaxes.parent)
+                    
                     .inner_join(Customer)
                     .on(Customer.name == SalesOrder.customer)
                     .where(SalesOrder.name == data.get('name'))
@@ -84,11 +83,17 @@ def get_sale_order(**data):
                         Customer.customer_id
                         ,SalesOrder.customer,SalesOrder.customer_name,SalesOrder.address_display,UNIX_TIMESTAMP(SalesOrder.delivery_date).as_('delivery_date'),SalesOrder.set_warehouse,SalesOrder.total,SalesOrder.grand_total
                         ,SalesOrder.taxes_and_charges,SalesOrder.total_taxes_and_charges, SalesOrder.apply_discount_on, SalesOrder.additional_discount_percentage,SalesOrder.discount_amount,SalesOrder.contact_person,SalesOrder.rounded_total
-                        , SalesOrderItem.item_name,SalesOrderItem.item_code,SalesOrderItem.qty, SalesOrderItem.uom,SalesOrderItem.amount,SalesOrderItem.discount_amount,SalesOrderItem.discount_percentage
-                        ,SalesOrderTaxes.tax_amount,SalesOrderTaxes.rate,SalesOrderTaxes.account_head,SalesOrderTaxes.charge_type,
+                        , SalesOrderItem.item_name,SalesOrderItem.item_code,SalesOrderItem.qty, SalesOrderItem.uom,SalesOrderItem.amount,SalesOrderItem.discount_amount,SalesOrderItem.discount_percentage                        
                     )
                     ).run(as_dict =1)
-            detail_order = {"list_items": [],'taxes': []}
+            
+            detail_taxes = (frappe.qb.from_(SalesOrder)
+                            .inner_join(SalesOrderTaxes)
+                            .on(SalesOrder.name == SalesOrderTaxes.parent)
+                            .where(SalesOrder.name == data.get('name'))
+                            .select(SalesOrderTaxes.tax_amount,SalesOrderTaxes.rate,SalesOrderTaxes.account_head,SalesOrderTaxes.charge_type,)
+                            ).run(as_dict =1)
+            detail_order = {"list_items": []}
             taxes = []
             for item in detail :
                 items_list = {}
@@ -102,7 +107,8 @@ def get_sale_order(**data):
                     #     taxes_list[key_item] = value
                 detail_order['list_items'].append(items_list)
                 # taxes.append(taxes_list)
-            # for taxe in taxes:
+            if len(detail_taxes) > 0 :
+                detail_order = {**detail_order,**detail_taxes[0]}
 
             gen_response(200,'',detail_order)
             return 
