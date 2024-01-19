2c72c1dc819aec029b5e09e517fac2e61b571b72
chuyendev <chuyendev@gmail.com>
2024-01-19 16:12:55 +0700

    createinventory

--- mbw_dms/api/checkin/__init__.py
+++ mbw_dms/api/checkin/__init__.py
@@ -2,7 +2,7 @@ import frappe
 from datetime import datetime
 from frappe.utils.data import get_time
 from mbw_dms.api.common import exception_handel, gen_response, get_language
-from mbw_dms.api.validators import validate_datetime
+from mbw_dms.api.validators import (validate_datetime,validate_filter)
 from mbw_dms.config_translate import i18n
 
 @frappe.whitelist(methods='POST')
@@ -37,3 +37,29 @@ def create_checkin(**kwargs):
         return gen_response(200, "Thành công", {"name": new_checkin.name})
     except Exception as e:
         return exception_handel(e)
+    
+
+@frappe.whitelist(methods='POST')
+def create_checkin_inventory(**body):
+    try:
+        normal_keys = [
+            "inventory_cus_name", "inventory_cus_id", "inventory_customer_name"
+            , "inventory_cus_address", "inventory_items"
+        ]
+        del body['cmd']
+        for key, value in body.items():
+            if key in normal_keys:
+                validate_filter(type_check='require',value=value)
+        items = body.get('inventory_items')
+        for item in items:
+            item['exp_time'] = validate_filter(type_check='date',value=item['exp_time'])
+        body['doctype'] = "DMS Inventory"
+        body['inventory_items'] = items
+        doc = frappe.get_doc(body)
+        print(doc)
+        doc.save()
+        frappe.db.commit()
+        return gen_response(200, "Thành công", {"name": doc.name})
+    except Exception as e:
+        return exception_handel(e)
+    
