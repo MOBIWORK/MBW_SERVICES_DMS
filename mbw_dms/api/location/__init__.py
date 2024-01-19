9ccce122a043ab2033e9b64a226df202a5ba8bf5
Bi100401 <phongtran100401@gmail.com>
2024-01-19 15:54:44 +0700

    update

--- mbw_dms/api/location/__init__.py
+++ mbw_dms/api/location/__init__.py
@@ -5,7 +5,7 @@ from mbw_dms.api.common import (
     exception_handel,
     gen_response,
 )
-
+from mbw_dms.api.validators import validate_not_none
 # Danh sánh tỉnh/thành phố
 @frappe.whitelist(methods='GET')
 def list_province(**kwargs):
@@ -20,7 +20,8 @@ def list_province(**kwargs):
 @frappe.whitelist(methods='GET')
 def list_district(ma_tinh):
     try:
-        list_districts = frappe.db.get_list('DMS District', filters={'ma_tinh_thanh': ma_tinh}, fields=['ma_huyen', 'ten_huyen', 'ma_tinh_thanh'], order_by='ma_huyen asc')
+        
+        list_districts = frappe.db.get_list('DMS District', filters={'ma_tinh_thanh': validate_not_none(ma_tinh)}, fields=['ma_huyen', 'ten_huyen', 'ma_tinh_thanh'], order_by='ma_huyen asc')
         gen_response(200, 'Thành công', list_districts)
     except Exception as e:
         return exception_handel(e)
@@ -30,7 +31,7 @@ def list_district(ma_tinh):
 @frappe.whitelist(methods='GET')
 def list_ward(ma_quan_huyen):
     try:
-        list_wards = frappe.db.get_list('DMS Ward', filters={'ma_quan_huyen': ma_quan_huyen}, fields=['ma_xa', 'ten_xa', 'ma_quan_huyen'], order_by='ma_xa asc')
+        list_wards = frappe.db.get_list('DMS Ward', filters={'ma_quan_huyen': validate_not_none(ma_quan_huyen)}, fields=['ma_xa', 'ten_xa', 'ma_quan_huyen'], order_by='ma_xa asc')
         gen_response(200, 'Thành công', list_wards)
     except Exception as e:
         return exception_handel(e)
