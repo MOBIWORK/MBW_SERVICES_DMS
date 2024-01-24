2bbbd79c0ed2fff179ba566c70b2dbdfc0869d36
chuyendev <chuyendev@gmail.com>
2024-01-24 14:45:55 +0700

    update primary address customer

--- mbw_dms/api/checkin/__init__.py
+++ mbw_dms/api/checkin/__init__.py
@@ -4,6 +4,7 @@ from frappe.utils.data import get_time
 from mbw_dms.api.common import exception_handel, gen_response, get_language ,get_user_id,upload_image_s3
 from mbw_dms.api.validators import (validate_datetime,validate_filter)
 from mbw_dms.config_translate import i18n
+import json
 
 @frappe.whitelist(methods='POST')
 def create_checkin(**kwargs):
@@ -93,9 +94,61 @@ def create_checkin_image(**body):
     except Exception as e:
         exception_handel(e)
 
+# from frappe.model.naming import make_autoname
 @frappe.whitelist(methods='PATCH')
-def update_address_customer():
+def update_address_customer(**body):
     try:
-        print('address')
+        customer = validate_filter(type_check='require',value=body.get('customer'))
+        city = validate_filter(type_check='require',value=body.get('city'))
+        county = validate_filter(type_check='require',value=body.get('county'))
+        country = validate_filter(type_check='require',value=body.get('country'))
+        state = validate_filter(type_check='require',value=body.get('state'))
+        address_line1 = validate_filter(type_check='require',value=body.get('address_line1'))
+        long = validate_filter(type_check='require',value=body.get('long'))
+        lat = validate_filter(type_check='require',value=body.get('lat'))
+        address_location = json.dumps({"long": long,"lat":lat})
+
+        customer_info = frappe.db.get_value(doctype="Customer",filters= {"name": customer},fieldname=['name','customer_primary_address',"customer_name"],as_dict=1)
+        doc_customer = frappe.get_doc("Customer",body.get('customer'))
+        if customer_info:
+            city_info = frappe.db.get_value(doctype="DMS Province",filters={"ten_tinh": ["like",f"%{city}%"]},fieldname=['ma_tinh'])
+            district_info = frappe.db.get_value(doctype="DMS District",filters={"ten_huyen": ["like",f"%{county}%"]},fieldname=['ma_huyen'])
+            ward_info = frappe.db.get_value(doctype="DMS Ward",filters={"ten_xa": ["like",f"%{state}%"]},fieldname=['name'])
+            new_address = {
+                    
+                    "address_title": f"{address_line1}, {state}, {county}, {city}, {country}",
+                    "address_line1":address_line1,                    
+                    "state": ward_info,
+                    "county": district_info,
+                    "city": city_info,   
+                    "address_location":address_location               
+                }
+            if customer_info.get("customer_primary_address"):
+                # new_address.update({'name': f"{address_line1}, {state}, {county}, {city}"})
+                doc_address = frappe.get_doc("Address",customer_info.get("customer_primary_address"))
+                for key,value in new_address.items():
+                    setattr(doc_address,key,value)
+                doc_address.save()
+            else: 
+                new_address.update({  
+                    "doctype": "Address",                  
+                     "links": [{
+                        "link_doctype": "Customer",
+                        "link_name":customer,
+                        "link_title": customer_info.get("customer_name")
+                    }],
+                    "address_type": "Billing"
+                })
+                doc_address = frappe.get_doc(new_address)
+                doc_address.save()
+                setattr(doc_customer,"customer_primary_address",doc_address.get('name'))
+                doc_customer.save()
+            
+            frappe.db.commit()
+            gen_response(200,"",doc_address.get('address_title') )
+            return
+        else:
+            gen_response(406,i18n.t('translate.not_found', locale=get_language()),False)             
+        
     except Exception as e:
         exception_handel(e)
