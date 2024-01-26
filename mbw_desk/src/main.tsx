0fedd98e5821d601f8a45e4f3d9f682f17e2bc11
Bi100401 <phongtran100401@gmail.com>
2024-01-26 09:59:46 +0700

    update

--- mbw_desk/src/main.tsx
+++ mbw_desk/src/main.tsx
@@ -7,6 +7,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BASE_URL } from './routes/path.ts';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 
