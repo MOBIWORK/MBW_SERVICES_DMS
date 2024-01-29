69a1579d09c4401801dff748e20886402d5e2cc2
Bi100401 <phongtran100401@gmail.com>
2024-01-29 15:57:54 +0700

    fix build

--- mbw_dms/public/mbw_fe/assets/RouterEmployee-C8390UWy.js
+++
@@ -1,1 +0,0 @@
-import{a9 as e,ab as r}from"./index-PSZGKfBk.js";function t(){return e.jsx("div",{children:"RouterEmployee"})}function o(){return e.jsxs(e.Fragment,{children:[e.jsx(r,{children:e.jsx("title",{children:" RouterControll"})}),e.jsx(t,{})]})}export{o as default};
