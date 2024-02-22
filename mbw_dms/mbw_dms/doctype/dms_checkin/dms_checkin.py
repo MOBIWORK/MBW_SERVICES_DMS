43078e18224abb2ad591ed12060dd70868257a4d
Tuan Bui <antivenomff@gmail.com>
2024-02-22 08:40:12 +0700

    cache api

--- mbw_dms/mbw_dms/doctype/dms_checkin/dms_checkin.py
+++ mbw_dms/mbw_dms/doctype/dms_checkin/dms_checkin.py
@@ -132,7 +132,7 @@ def create_checkin(kwargs):
             elif key in date_keys:
                 created_date = validate_datetime(value)
                 new_checkin.set(key, created_date)
-        new_checkin.insert()
+        new_checkin.insert(ignore_permissions=True)
         frappe.db.commit()
         return gen_response(201, "Thành công", {"name": new_checkin.name})
     except Exception as e:
--- mbw_dms/mbw_dms/doctype/dms_config_mobile/dms_config_mobile.py
+++ mbw_dms/mbw_dms/doctype/dms_config_mobile/dms_config_mobile.py
@@ -6,12 +6,16 @@ from frappe.model.document import Document
 from mbw_dms.api.common import exception_handel, gen_response
 
 class DMSConfigMobile(Document):
-	pass
+    def on_update(self):
+        frappe.cache.delete_value('website-config')
 
 @frappe.whitelist(methods='GET')
 def get_list_config_mobile():
     try:
-        list_configs = frappe.get_doc('DMS Config Mobile', 'DMS Config Mobile')
-        gen_response(200, 'Thành công', list_configs)
+        list_configs = frappe.cache.get_value("website-config")
+        if list_configs == None:
+            list_configs = frappe.get_doc('DMS Config Mobile', 'DMS Config Mobile')
+            frappe.cache.set_value("website-config", list_configs)
+        return gen_response(200, 'Thành công', list_configs)
     except Exception as e:
         return exception_handel(e)
