84e500ac510d598780bdae1789a84ef1825e90d2
chuyendev <chuyendev@gmail.com>
2024-02-28 08:32:06 +0700

    update fix create checkin image

--- mbw_dms/api/helpers/optimal_router.py
+++ mbw_dms/api/helpers/optimal_router.py
@@ -6,31 +6,57 @@ from mbw_dms.api.common import (
     gen_response,
     get_language,
 )
-from mbw_service_v2.utils import OPTIMAL_ROUTER
+from mbw_dms.utils import OPTIMAL_ROUTER
 import requests
 import json
 from mbw_dms.config_translate import i18n
 
 # toi uu tuyen
-@frappe.whitelist(methods="GET", allow_guest=True)
+@frappe.whitelist(methods="POST", allow_guest=True)
 def optimal_router(**kwargs):
     try:
-        list_location = kwargs.get('locations')
-        settings = frappe.db.get_singles_dict("MBW Employee Settings")
-        geo_service = settings.get("geo_service")
-
-        if geo_service == "Ekgis":
-            key = settings.get("api_key_ekgis")
-            url = f"{OPTIMAL_ROUTER.get('EKGIS')}/{list_location}?api_key={key}"
+        list_location = kwargs.get('locations',[])
+        if len(list_location) <=1:
+            return gen_response(200,"",list_location)
+        settings = frappe.db.get_singles_dict("DMS Setting")
+        select_service = settings.get("select_service")
+        company_long = float(settings["company_long"])
+        company_lat = float(settings["company_lat"])
+        print("select_service",select_service)
+        if select_service == "EKgis":
+            key = settings.get("api_key")
+            url = f"{OPTIMAL_ROUTER.get('EKGIS')}?api_key={key}"
         else:
-            key = settings.get("api_key_google")
-            url = f"{OPTIMAL_ROUTER.get('GOOGLE')}/{list_location}?api_key={key}"
+            key = settings.get("api_key")
+            url = f"{OPTIMAL_ROUTER.get('GOOGLE')}?api_key={key}"
 
-        if not key or not geo_service:
+        if not key:
             return gen_response(400, i18n.t('translate.not_found_setting_map', locale=get_language()))
+        
+        jobs = []
+        vehicles = [
+            {
+                "id":1,
+                "profile": "driving-car",
+                "start":[company_long, company_lat],
 
-        # call geolocation
-        response = requests.get(url)
+            }
+        ]
+        for location in list_location :
+            jobs.append({
+                    "id": location["id"],
+                    "location":[location["long"], location["lat"]]
+                })
+       
+        data = {
+            "jobs": jobs,
+            "vehicles": vehicles
+        }
+        response = requests.post(url,headers={'Content-Type': 'application/json'},data=json.dumps(data))
+        data_return  =  json.loads(response.text)
+        if data_return.error:
+            return gen_response(500,data_return.error,[])
+        
         return gen_response(200, i18n.t('translate.successfully', locale=get_language()), json.loads(response.text))
     except Exception as e:
         return exception_handel(e)
---
+++ mbw_dms/utils.py
@@ -0,0 +1,4 @@
+OPTIMAL_ROUTER = {
+    "EKGIS": "https://api.ekgis.vn/navigation/vrp",
+    "GOOGLE": ""
+}
