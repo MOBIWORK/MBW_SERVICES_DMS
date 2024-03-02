9f4b031da6a44a774577873e4c60d67d487ac5af
Tuan Bui <antivenomff@gmail.com>
2024-03-02 10:32:15 +0700

    report kpi

--- mbw_dms/api/kpi/__init__.py
+++ mbw_dms/api/kpi/__init__.py
@@ -4,3 +4,18 @@ import frappe
 def get_kpi_monthly():
     from mbw_dms.mbw_dms.doctype.dms_summary_kpi_monthly.dms_summary_kpi_monthly import get_kpi_monthly
     return get_kpi_monthly()
+
+@frappe.whitelist()
+def visit_report():
+    from mbw_dms.mbw_dms.doctype.dms_kpi.dms_kpi import visit_report
+    return visit_report()
+
+@frappe.whitelist()
+def sales_report():
+    from mbw_dms.mbw_dms.doctype.dms_kpi.dms_kpi import sales_report
+    return sales_report()
+
+@frappe.whitelist()
+def invoices_report():
+    from mbw_dms.mbw_dms.doctype.dms_kpi.dms_kpi import invoices_report
+    return invoices_report()
