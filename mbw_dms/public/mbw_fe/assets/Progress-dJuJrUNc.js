bc55d74a62be667c33c7f5bf97ffc93e84f6c4d5
chuyendev <chuyendev@gmail.com>
2024-03-11 10:37:29 +0700

    change doc routre customer

--- mbw_dms/public/mbw_fe/assets/Progress-dJuJrUNc.js
+++
@@ -1,1 +0,0 @@
-import{j as s,H as t,b6 as e}from"./index-vtiinQOu.js";function n(){return s.jsxs(s.Fragment,{children:[s.jsx(t,{children:s.jsx("title",{children:"Progress"})}),s.jsx(e,{status:"500",title:"500",subTitle:"Trang đang tiến hành."})]})}export{n as default};
