fe69376e1f9ffd06cdd68fafb336bd3eb562d1ec
chuyendev <chuyendev@gmail.com>
2024-03-21 16:13:28 +0700

    un comment monio upload image

--- mbw_dms/api/common.py
+++ mbw_dms/api/common.py
@@ -9,9 +9,9 @@ import base64
 from frappe.core.doctype.file.utils import delete_file
 from frappe.utils.file_manager import save_file
 
-# from mbw_dms.api.file import (
-#     my_minio
-# )
+from mbw_dms.api.file import (
+    my_minio
+)
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
