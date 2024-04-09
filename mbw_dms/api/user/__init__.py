3c8c1b090dd8221ece3ec8a606e73620ff19c767
Tuan Bui <antivenomff@gmail.com>
2024-04-09 14:52:28 +0700

    api get prjID, objID

--- mbw_dms/api/user/__init__.py
+++ mbw_dms/api/user/__init__.py
@@ -1,7 +1,7 @@
 import frappe
 from frappe import _
 from frappe.utils.password import update_password, check_password
-from mbw_dms.api.common import gen_response
+from mbw_dms.api.common import gen_response, exception_handle
 
 @frappe.whitelist(methods='PUT')
 def change_password(user, current_password, new_password):
@@ -19,3 +19,39 @@ def change_password(user, current_password, new_password):
     update_password(user, new_password)
 
     return gen_response(200, "Cập nhật thành công")
+
+# Lấy project ID
+@frappe.whitelist(methods='GET')
+def get_projectID(**kwargs):
+    try:
+        projectID = ''
+        dms_settinngs = frappe.get_doc("DMS Settings").as_dict()
+        if dms_settinngs.get('ma_du_an'):
+            projectID = dms_settinngs.get('ma_du_an')
+
+        return gen_response(200, 'Thành công', {
+            'Project ID': projectID
+        })
+    except Exception as e:
+        return exception_handle(e)
+    
+# Lấy project ID và Object ID
+@frappe.whitelist(methods='GET')
+def get_project_object_id(employee_id):
+    try:
+        projectID = ''
+        dms_settinngs = frappe.get_doc("DMS Settings").as_dict()
+        if dms_settinngs.get('ma_du_an'):
+            projectID = dms_settinngs.get('ma_du_an')
+
+        objectId = ''
+        user_name = frappe.db.get_list('Employee', filters={'user_id': employee_id}, fields=['object_id'])
+        if user_name[0]['object_id'] is not None:
+            objectId = user_name[0]['object_id']
+
+        return gen_response(200, 'Thành công', {
+            'Project ID': projectID,
+            'Object ID': objectId
+        })
+    except Exception as e:
+        return exception_handle(e)
