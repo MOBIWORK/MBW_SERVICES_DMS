b434a8a775bf12dd72a83d0fdd70464d51465ba3
Bi100401 <phongtran100401@gmail.com>
2024-04-20 10:58:33 +0700

    update field

--- mbw_dms/mbw_dms/doctype/dms_album/dms_album.js
+++ mbw_dms/mbw_dms/doctype/dms_album/dms_album.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2024, MBW and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('DMS Album', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on("DMS Album", {
+  setup: function (frm) {
+    frm.set_query("nhom_ban_hang", function () {
+      return {
+        filters: { is_group: 1 },
+      };
+    });
+  },
 });
