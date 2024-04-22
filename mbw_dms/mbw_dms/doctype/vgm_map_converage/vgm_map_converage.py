70c3a945ab35cae1cf1983c724256c01f94b7b67
dangduy1998 <duycong383@gmail.com>
2024-04-22 18:20:41 +0700

    add setting map converage

---
+++ mbw_dms/mbw_dms/doctype/vgm_map_converage/vgm_map_converage.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, MBW and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class VGM_Map_Converage(Document):
+	pass
