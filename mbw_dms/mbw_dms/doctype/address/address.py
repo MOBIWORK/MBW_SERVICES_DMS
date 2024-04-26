5af0c4b63931006b0eb30995f104ddb53b0f5f95
chuyendev <chuyendev@gmail.com>
2024-04-26 16:09:34 +0700

    address

--- mbw_dms/mbw_dms/doctype/address/address.py
+++ mbw_dms/mbw_dms/doctype/address/address.py
@@ -18,15 +18,16 @@ def update_address(doc,method=None):
 			if customer and customer.customer_primary_address == doc.name :
 				customer.customer_location_primary = doc.address_location
 				customer.save()
-	from  frappe.model.rename_doc import update_document_title
-	update_document_title(doctype= "Address",
-							docname= doc.name,
-							name= doc.address_title,
-							title= doc.address_title,
-							enqueue= True,
-							merge= 0,
-							freeze= True,
-							freeze_message= "Updating related fields..."
-						)
+	if doc.is_new() == False:
+		from  frappe.model.rename_doc import update_document_title
+		update_document_title(doctype= "Address",
+								docname= doc.name,
+								name= doc.address_title,
+								title= doc.address_title,
+								enqueue= True,
+								merge= 0,
+								freeze= True,
+								freeze_message= "Updating related fields..."
+							)
 	frappe.db.commit()
 	pass
