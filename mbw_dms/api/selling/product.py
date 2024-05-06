b21931aafab1bbf53ea95652a0a07cb4c4952d32
chuyendev <chuyendev@gmail.com>
2024-05-06 11:31:34 +0700

    add [200~list_product_campaign~

--- mbw_dms/api/selling/product.py
+++ mbw_dms/api/selling/product.py
@@ -86,7 +86,86 @@ def list_product(**kwargs):
     except Exception as e:
         return exception_handle(e)
 
-    
+
+#list product campaign
+@frappe.whitelist(methods='GET')
+def list_product_campaign(**kwargs):
+    try:
+        kwargs = frappe._dict(kwargs)
+        if not frappe.has_permission("Item"):
+            frappe.throw(_("Không có quyền"), frappe.PermissionError)
+
+        my_filter = {}
+        name = kwargs.get('name')
+        customer = kwargs.get('customer')
+        name_item = kwargs.get('item_name')
+        brand = kwargs.get('brand')
+        custom_industry = kwargs.get("industry")
+        item_group = kwargs.get("item_group")
+        page_size = kwargs.get('page_size', 20)
+        page_number = 1 if not kwargs.get('page') or int(kwargs.get('page')) <= 0 else int(kwargs.get('page'))
+
+        price_list = None
+        price_lisr_cg = None
+        default_price_list = frappe.get_doc('Selling Settings').selling_price_list
+        if customer:
+            customer_group = frappe.get_value('Customer', {'name': customer}, 'customer_group')
+            price_lisr_cg = frappe.get_value('Customer Group', {'name': customer_group}, 'default_price_list')
+        if price_lisr_cg:
+            price_list = price_lisr_cg
+        else:
+            price_list = default_price_list
+
+        if name:
+            my_filter["name"] = ['like', f'%{name}%']
+        if name_item:
+            my_filter["item_name"] = ['like', f'%{name_item}%']
+        if brand:
+            my_filter["brand"] = ['like', f'%{brand}%']
+        if custom_industry:
+            my_filter["custom_industry"] = ['like', f'%{custom_industry}%']
+        if item_group:
+            my_filter["item_group"] = ['like', f'%{item_group}%']
+
+        items = frappe.db.get_list("Item",
+                                   filters=my_filter,
+                                   fields=[
+                                            "name",
+                                            "item_code", "item_name", "item_group", 
+                                            "stock_uom", "min_order_qty", "description",
+                                            "brand", "country_of_origin", "image",
+                                            "custom_industry", "end_of_life"],
+                                   start=page_size * (page_number - 1),
+                                   page_length=page_size)
+        for item in items:
+            item_doc = frappe.get_doc("Item", item.get("name"))
+            images = item_doc.custom_images_item or []
+            def return_fiel(value):
+                return pydash.pick(value, "link_image")
+            images_links = pydash.map_(images, return_fiel)
+            item["custom_images_item"] = images_links
+        count = len(frappe.db.get_list("Item",
+                                   filters=my_filter))
+
+        data_item = []
+        for item in items:
+            item['image'] = validate_image(item.get("image"))
+            item['details'] = frappe.get_all("Item Price", filters={"item_code": item.get('item_code'), "price_list": price_list}, fields=['uom', 'price_list', 'price_list_rate', 'valid_from', 'currency'])
+            item['unit'] = frappe.db.get_all("UOM Conversion Detail", {"parent" : item.get('name')}, ['uom', 'conversion_factor'])
+            item['stock'] = frappe.db.get_all("Stock Entry Detail", {"item_code": item.get('item_code')}, ['t_warehouse', 'qty'])
+            data_item.append(item)
+
+        return gen_response(200, "Thành công", {
+            "data": data_item,
+            "total": count,
+            "page_size": page_size,
+            "page_number": page_number
+        })
+
+    except Exception as e:
+        return exception_handle(e)
+
+      
 #list brand
 @frappe.whitelist(methods="GET")
 def list_brand():
