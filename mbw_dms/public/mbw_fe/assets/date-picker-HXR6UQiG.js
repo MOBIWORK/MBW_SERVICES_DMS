faab582c8733c1e53ce12064a44bae4043a5a087
chuyendev <chuyendev@gmail.com>
2024-06-06 10:46:08 +0700

    fix auth

--- mbw_dms/public/mbw_fe/assets/date-picker-HXR6UQiG.js
+++
@@ -1,3 +0,0 @@
-import{bj as t}from"./index-JEQ7519Z.js";import{D as o}from"./index-ubqCdk63.js";const i=t(o)`
-    background: #F5F7FA !important;
-`;export{i as D};
---
+++ mbw_dms/public/mbw_fe/assets/index-ccGpMbvF.js
@@ -0,0 +1,1 @@
+import{P as e}from"./index-ZqUxRnba.js";function x({header:s,children:a}){return e.jsxs("div",{className:"max-h-screen flex flex-col",children:[e.jsx("div",{className:"w-full bg-white",children:s}),e.jsx("div",{className:"w-full p-5 flex-1 max-h-[calc(100vh-80px)] bg-[#f9fafa] ",children:a})]})}export{x as C};
