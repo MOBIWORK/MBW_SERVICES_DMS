be96d880a16fc9527322190b0a6c0157e7bf0f27
chuyendev <chuyendev@gmail.com>
2024-07-03 09:27:36 +0700

    push api log

---
+++ mbw_dms/api/logs/__init__.py
@@ -0,0 +1,12 @@
+import frappe
+from mbw_dms.api.common import gen_response
+@frappe.whitelist(methods="POST")
+def write_log(**kwagr):
+    fields_in = ["orgid","user","api","screen","detail"]
+    new_logs  =frappe.new_doc("DMS Error Logs")
+    for key,value in kwagr.items():
+        if key in fields_in:
+            new_logs.set(key,value)
+    new_logs.save()
+    return gen_response(200,"",{})
+    pass
