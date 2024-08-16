c7a61313d48c1d8be95a0ea4a71e68a0779c3598
Tuan Bui <antivenomff@gmail.com>
2024-08-16 14:21:04 +0700

    update kpi daily

--- mbw_dms/hooks.py
+++ mbw_dms/hooks.py
@@ -258,13 +258,13 @@ doc_events = {
         "after_insert": "mbw_dms.controllers.dms_users.create_objid_employee"
     },
     "Sales Invoice": {
-        "on_submit": "mbw_dms.controllers.dms_sales_invoice.update_kpi_monthly",
-        "on_cancel": "mbw_dms.controllers.dms_sales_invoice.update_kpi_monthly_on_cancel",
+        "on_submit": ["mbw_dms.controllers.dms_sales_invoice.update_kpi_monthly", "mbw_dms.controllers.dms_sales_invoice.update_kpi_daily"],
+        "on_cancel": ["mbw_dms.controllers.dms_sales_invoice.update_kpi_monthly_on_cancel", "mbw_dms.controllers.dms_sales_invoice.update_kpi_daily_on_cancel"],
         "before_insert": "mbw_dms.controllers.dms_sales_invoice.update_sales_person"
     },
     "Sales Order": {
-        "on_submit": "mbw_dms.controllers.dms_sales_order.update_kpi_monthly",
-        "on_cancel": "mbw_dms.controllers.dms_sales_order.update_kpi_monthly_on_cancel",
+        "on_submit": ["mbw_dms.controllers.dms_sales_order.update_kpi_monthly", "mbw_dms.controllers.dms_sales_order.update_kpi_daily"],
+        "on_cancel": ["mbw_dms.controllers.dms_sales_order.update_kpi_monthly_on_cancel", "mbw_dms.controllers.dms_sales_order.update_kpi_daily_on_cancel"],
         "before_insert": "mbw_dms.controllers.dms_sales_order.update_sales_person"
     },
     "DMS Router": {
