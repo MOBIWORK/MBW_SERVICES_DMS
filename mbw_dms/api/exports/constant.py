6ce2ef1361906d60d25eb9fb3056beec295646a8
chuyendev <chuyendev@gmail.com>
2024-08-22 14:26:51 +0700

    fix not have phone contact

--- mbw_dms/api/exports/constant.py
+++ mbw_dms/api/exports/constant.py
@@ -1,7 +1,7 @@
 template_header = {
     "Report KPI" : "mbw_dms.mbw_dms.doctype.dms_kpi.dms_kpi.report",
     "Report Inventory": "",
-    "Report Checkin": "",
+    "Report Checkin": "mbw_dms.mbw_dms.doctype.dms_checkin.dms_checkin.get_report",
     "Report Sell": "",
     "Report Order": "",
     "Report Customer": "",
