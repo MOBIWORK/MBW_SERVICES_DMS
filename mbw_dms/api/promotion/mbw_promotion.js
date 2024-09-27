c9efa234951965efff74a01b57fcf04c381834f7
MOBIWORK <minhvq@mobiwork.vn>
2024-09-27 18:32:48 +0700

    Tungda update promotion start

---
+++ mbw_dms/api/promotion/mbw_promotion.js
@@ -0,0 +1,116 @@
+frappe.ui.form.on('Sales Order', {
+    onload_post_render: function(frm) {
+        frm.set_value('ignore_pricing_rule', 1);
+    },
+    refresh: function(frm) {
+        // Chỉ hiển thị button khi tạo mới Sales Order
+        if (frm.doc.docstatus == 0) {
+            frm.add_custom_button(__('Get Promotion'), function() {
+                onClearPromotion(frm); // Clear free items and set ignore pricing rule
+                
+            }).addClass('btn-primary');
+        }
+    }
+});
+function processAfterApplyPromotion(ptype_value,result,frm)
+{
+    if(ptype_value=="SP_SL_CKSP")
+    {
+        for(var i=0;i<result.length;i++)
+        {
+            let filtered_items = frm.doc.items.find(function(item) {
+                return item.item_code === result[i].item_code;
+            });
+            if (filtered_items) {
+                
+                // Recalculate the amount based on the new quantity
+                filtered_items.discount_amount = result[i].discount_amount;
+                // Trigger the onchange event for the discount_amount field
+                frm.script_manager.trigger("discount_amount", filtered_items.doctype, filtered_items.name);
+
+                // Refresh the items table to reflect the changes
+                frm.refresh_field('items');
+            }
+        }
+        
+    }
+}
+function onClearPromotion(frm) {
+    // Set ignore pricing rule to 1
+    frm.set_value('ignore_pricing_rule', 1);
+
+    // Remove free items (for example, items with rate == 0 or custom is_free_item == 1)
+    frm.doc.items = frm.doc.items.filter(function(item) {
+        return item.rate > 0 || !item.is_free_item; // Assuming is_free_item is a custom field for free items
+    });
+    // Refresh the items field after removing free items
+    frm.refresh_field('items');
+    // Cap nhat lai gia sau khi chay km
+    update_item_prices_by_price_list(frm)
+}
+
+function update_item_prices_by_price_list(frm) {
+    let total_amount = 0;
+    frm.doc.items.forEach(function(item) {
+        frappe.call({
+            method: "erpnext.stock.get_item_details.get_item_details",
+            args: {
+                args: {
+                    item_code: item.item_code,
+                    serial_no: item.serial_no,
+                    batch_no: item.batch_no,
+                    set_warehouse: frm.doc.set_warehouse,
+                    warehouse: item.warehouse,
+                    customer: frm.doc.customer || frm.doc.party_name,
+                    currency: frm.doc.currency,
+                    conversion_rate: frm.doc.conversion_rate,
+                    price_list: frm.doc.selling_price_list || frm.doc.buying_price_list,
+                    price_list_currency: frm.doc.price_list_currency,
+                    plc_conversion_rate: frm.doc.plc_conversion_rate,
+                    company: frm.doc.company,
+                    order_type: frm.doc.order_type,
+                    ignore_pricing_rule: frm.doc.ignore_pricing_rule,
+                    doctype: frm.doc.doctype,
+                    name: frm.doc.name,
+                    project: item.project || frm.doc.project,
+                    qty: item.qty || 1,
+                    net_rate: item.rate,
+                    base_net_rate: item.base_net_rate,
+                    stock_qty: item.stock_qty,
+                    weight_per_unit: item.weight_per_unit,
+                    uom: item.uom,
+                    weight_uom: item.weight_uom,
+                    stock_uom: item.stock_uom,
+                    tax_category: frm.doc.tax_category,
+                    item_tax_template: item.item_tax_template,
+                    child_doctype: item.doctype,
+                    child_docname: item.name,
+                }
+            },
+            callback: function (r) {
+                
+                if (!r.exc) {
+                    item.rate = r.message.price_list_rate;
+                    item.amount = item.rate * item.qty;
+                    total_amount += item.amount;
+                    frm.set_value('total', total_amount);
+                    refresh_field("items");
+                    frm.refresh_field('total');
+                    // Gọi một server-side method để lấy khuyến mãi
+                    frappe.call({
+                        method: 'mbw_dms.api.promotion.mbw_promotion.get_list_promotion',
+                        args: {
+                            listItem:frm.doc.items
+                        },
+                        callback: function(r) {
+                            if (r.message) {
+                                processAfterApplyPromotion(r.message.ptype_value,r.message.result,frm)
+                            }
+                        }
+                    });
+                }
+            },
+        });
+    });
+    
+}
--- mbw_dms/api/promotion/mbw_promotion.py
+++ mbw_dms/api/promotion/mbw_promotion.py
@@ -1,13 +1,19 @@
 import frappe
 import json
 from frappe.utils import nowdate
-
+import pydash
+#Get All Promotion By Fitler
+@frappe.whitelist()
 def get_list_promotion(**kwargs):
     filters = []
     customer = kwargs.get("customer")
     territory = kwargs.get("territory")
+    list_item=kwargs.get("listItem")
     date = nowdate()
-
+    # If list_Item is a string, convert it to a list using json.loads()
+    if isinstance(list_item, str):
+        list_item = json.loads(list_item)
+          
     if customer:
         filters.append(f"cus.customer_code={frappe.db.escape(customer)}")
     if territory:
@@ -24,7 +30,6 @@ def get_list_promotion(**kwargs):
 
     if where_conditions:
         sql_query += f" WHERE {where_conditions}"
-
     list_promotions = frappe.db.sql(sql_query, as_dict=True)
 
     # Loại bỏ các trường không mong muốn
@@ -43,4 +48,82 @@ def get_list_promotion(**kwargs):
             except json.JSONDecodeError:
                 promo['products'] = []
 
-    return list_promotions
+    #Tính toán CTKM
+    return apply_Promotion(list_item,list_promotions)
+    
+#Reset data truoc khi goi khuyen mai
+
+#Caculate Promotion
+def apply_Promotion(list_item=[],list_promotions=[]):
+    for promtion in list_promotions:
+        return caculate_promotion(list_item,promtion)
+    
+#Caculate Promotion
+def caculate_promotion(list_item=[],promtion={}):
+    if promtion.get("ptype_value")=="SP_SL_CKSP":
+        ref=SP_SL_CKSP(list_item,promtion)
+        return {"ptype_value":"SP_SL_CKSP","result":ref}
+
+    
+
+#"TuanBD Tổng tiền hàng - chiết khấu đơn hàng (%)"
+def TIEN_CKDH():
+    pass
+
+#"TuanBD Tổng tiền hàng - tặng tiền"
+def TIEN_TIEN():
+    pass
+
+#"TuanBD Tổng tiền hàng - tặng SP"
+def TIEN_SP():
+    pass
+
+#"TungDA Mua sản phẩm - đạt số lượng - chiết khấu SP (%)"
+def SP_SL_CKSP(list_Item=[],data_Promotion={}):
+    list_Item_Return=[]
+    getProductPromotion={}
+
+    for item in list_Item:
+        product = pydash.filter_(data_Promotion['products'], {'_id': item["item_code"]})
+        #Sap xep san pham theo yeu cau
+        product = pydash.sort_by(product, 'yeu_cau')
+        product.reverse()
+        #Ket qua khuyen mai dat duoc
+        
+        for prd in product:
+            if bool(prd["yeu_cau_min"]) and prd["yeu_cau_min"]!=0:
+                if item["qty"]>=prd["yeu_cau_min"] and item["uom"]==prd["don_vi_tinh"].get("choice_values"):
+                    getProductPromotion=prd
+                    break
+            else:
+                if item["qty"]>=prd["yeu_cau"] and item["uom"]==prd["don_vi_tinh"].get("choice_values"):
+                    getProductPromotion=prd
+                    break
+        if bool(getProductPromotion):
+            if item["qty"]>=getProductPromotion["yeu_cau"] and item["uom"]==getProductPromotion["don_vi_tinh"].get("choice_values"):
+                chietKhau_promotion=item["amount"]*getProductPromotion["khuyen_mai"]/100
+                item["discount_amount"]=chietKhau_promotion
+    return list_Item
+
+#"TungDA Mua sản phẩm - đạt số tien - chiết khấu SP (%)"
+def SP_ST_CKSP():
+    pass
+
+#"TungDA Mua sản phẩm - đạt số tiền - tặng sản phẩm"
+def SP_ST_SP():
+    pass
+
+#"TungDA Mua sản phẩm - đạt số tiền - tặng tiền"
+def SP_ST_TIEN():
+    pass
+
+#"TungDA Mua sản phẩm - đạt số luong - tặng tiền"
+def SP_SL_TIEN():
+    pass
+
+#"TungDA Mua sản phẩm - đạt số lượng - tặng sản phẩm"
+def SP_SL_SP():
+    pass
+
+
+
