5a75a564c280ca603d878927eacafb9ac74cebb9
trinhpham1605 <trinhpham1605@gmail.com>
2024-10-14 16:41:42 +0700

    fix-name-báo-cáo-xuất-nhập-tồn

--- mbw_dms/mbw_dms/report/báo_cáo_xuất_nhập_tồn_theo_sản_phẩm/báo_cáo_xuất_nhập_tồn_theo_sản_phẩm.js
+++ mbw_dms/mbw_dms/report/báo_cáo_xuất_nhập_tồn_theo_sản_phẩm/báo_cáo_xuất_nhập_tồn_theo_sản_phẩm.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, MBW and contributors
 // For license information, please see license.txt
 
-frappe.query_reports["Báo cáo sổ kho theo nhóm sản phẩm"] = {
+frappe.query_reports["Báo cáo xuất nhập tồn theo sản phẩm"] = {
 	filters: [
 		{
 			fieldname: "company",
