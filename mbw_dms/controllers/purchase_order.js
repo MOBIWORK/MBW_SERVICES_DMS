1883304e975c0b350f1f4c0a6e7aa434bc92fdf5
trinhpham1605 <129782337+trinhpham1605@users.noreply.github.com>
2024-11-12 08:07:17 +0700

    Update-purchase_order.js

--- mbw_dms/controllers/purchase_order.js
+++ mbw_dms/controllers/purchase_order.js
@@ -2,7 +2,7 @@ frappe.ui.form.on('Purchase Order', {
     refresh: function(frm) {
         if (frm.doc.name.includes("new-purchase-order") || frm.doc.status === "Draft") {
             frm.doc.items.forEach(item => {
-                if (item.last_purchase_rate !== null && item.last_purchase_rate !== '') {
+                if (item.last_purchase_rate !== null && item.last_purchase_rate !== '' && item.last_purchase_rate > 0) {
                     setTimeout(() => {
                         item.rate = item.last_purchase_rate;
                         item.amount = item.rate * item.qty;
@@ -21,7 +21,7 @@ frappe.ui.form.on('Purchase Order Item', {
         frappe.model.with_doc('Item', item.item_code, function() {
             item.last_purchase_rate = frappe.model.get_value('Item', item.item_code, 'last_purchase_rate');
 
-            if (item.last_purchase_rate) {
+            if (item.last_purchase_rate !== null && item.last_purchase_rate !== '' && item.last_purchase_rate > 0) {
                 setTimeout(() => {
                     frappe.model.set_value(cdt, cdn, 'rate', item.last_purchase_rate);
                 }, 150);
