b5cd5a1e7d7911d1409d102b90a1a852473e70bc
trinhpham1605 <trinhpham1605@gmail.com>
2024-12-14 10:12:47 +0700

    fix-status-delivery-so

--- mbw_dms/controllers/dms_delivery_trip.py
+++ mbw_dms/controllers/dms_delivery_trip.py
@@ -9,7 +9,7 @@ def update_delivery_status_so(doc, method):
                 if item.against_sales_order not in list_so:
                     list_so.append(item.against_sales_order)
             sales_orders = frappe.get_list("Sales Order", filters={"name": ["in", list_so]},
-                                           fields=["name", "custom_trạng_thái_giao_hàng"])
+                                           fields=["name", "custom_trạng_thái_giao_hàng", "custom_số_lượng_đã_giao", "total_qty"])
             if delivery_note.custom_status_delivery_trip == "Chưa tạo phiếu giao hàng":
                 delivery_note.custom_status_delivery_trip = "Đã tạo phiếu giao hàng"
                 delivery_note.save()
@@ -19,17 +19,45 @@ def update_delivery_status_so(doc, method):
                         sales_order.custom_trạng_thái_giao_hàng = "Đang giao hàng"
                         frappe.db.set_value("Sales Order", sales_order.name, "custom_trạng_thái_giao_hàng", "Đang giao hàng")
 
-            if d.visited == 1:
+            if d.visited == 1 and d.custom_updated_on_so == 0:
                 for sales_order in sales_orders:
-                    if sales_order.custom_trạng_thái_giao_hàng == "Đang giao hàng":
-                        frappe.db.set_value("Sales Order", sales_order.name, "custom_trạng_thái_giao_hàng", "Đã giao hàng")
+                    if sales_order.custom_trạng_thái_giao_hàng in ["Đang giao hàng", "Đã giao một phần"]:
+                        for item in delivery_note.items:
+                            if item.against_sales_order == sales_order.name:
+                                qty_delivery = sales_order.custom_số_lượng_đã_giao if sales_order.custom_số_lượng_đã_giao else 0
+                                qty_delivery += item.qty
+                                sales_order.custom_số_lượng_đã_giao = qty_delivery
+                                frappe.db.set_value("Sales Order", sales_order.name, "custom_số_lượng_đã_giao", qty_delivery)
+                                if qty_delivery > 0 and qty_delivery < sales_order.total_qty:
+                                    sales_order.custom_trạng_thái_giao_hàng = "Đã giao một phần"
+                                    frappe.db.set_value("Sales Order", sales_order.name, "custom_trạng_thái_giao_hàng",
+                                                    "Đã giao một phần")
+                                elif qty_delivery > 0 and qty_delivery == sales_order.total_qty:
+                                    sales_order.custom_trạng_thái_giao_hàng = "Đã giao đầy đủ"
+                                    frappe.db.set_value("Sales Order", sales_order.name, "custom_trạng_thái_giao_hàng",
+                                                        "Đã giao đầy đủ")
+                d.custom_updated_on_so = 1
+                frappe.db.set_value("Delivery Stop", d.name, "custom_updated_on_so", 1)
                 if not delivery_note.custom_image or delivery_note.custom_image == "" or delivery_note.custom_image != d.custom_add_image:
                     delivery_note.custom_image = d.custom_add_image
                     delivery_note.save()
-            if d.visited == 0:
+            if d.visited == 0 and d.custom_updated_on_so == 1:
                 for sales_order in sales_orders:
-                    if sales_order.custom_trạng_thái_giao_hàng == "Đã giao hàng":
-                        frappe.db.set_value("Sales Order", sales_order.name, "custom_trạng_thái_giao_hàng", "Đang giao hàng")
+                    if sales_order.custom_trạng_thái_giao_hàng in ["Đã giao đầy đủ", "Đã giao một phần"]:
+                        for item in delivery_note.items:
+                            if item.against_sales_order == sales_order.name:
+                                qty_delivery = sales_order.custom_số_lượng_đã_giao - item.qty if sales_order.custom_số_lượng_đã_giao - item.qty > 0 else 0
+                                sales_order.custom_số_lượng_đã_giao = qty_delivery
+                                frappe.db.set_value("Sales Order", sales_order.name, "custom_số_lượng_đã_giao",
+                                                    qty_delivery)
+                                if qty_delivery > 0 and qty_delivery < sales_order.total_qty:
+                                    frappe.db.set_value("Sales Order", sales_order.name, "custom_trạng_thái_giao_hàng",
+                                                        "Đã giao một phần")
+                                elif qty_delivery == 0:
+                                    frappe.db.set_value("Sales Order", sales_order.name, "custom_trạng_thái_giao_hàng",
+                                                        "Đang giao hàng")
+                d.custom_updated_on_so = 0
+                frappe.db.set_value("Delivery Stop", d.name, "custom_updated_on_so", 0)
                 if delivery_note.custom_image != None and delivery_note.custom_image != "":
                     delivery_note.custom_image = None
                     delivery_note.save()
@@ -44,7 +72,21 @@ def update_delivery_status_so(doc, method):
                 if item.against_sales_order not in list_so:
                     list_so.append(item.against_sales_order)
             sales_orders = frappe.get_list("Sales Order", filters={"name": ["in", list_so]},
-                                           fields=["name", "custom_trạng_thái_giao_hàng"])
-            for sales_order in sales_orders:
-                if sales_order.custom_trạng_thái_giao_hàng == "Đang giao hàng":
-                    frappe.db.set_value("Sales Order", sales_order.name, "custom_trạng_thái_giao_hàng", "Chưa giao hàng")
+                                           fields=["name", "custom_trạng_thái_giao_hàng", "custom_số_lượng_đã_giao", "total_qty"])
+            if d.custom_updated_on_so == 1:
+                for sales_order in sales_orders:
+                    if sales_order.custom_trạng_thái_giao_hàng != "Chưa giao hàng":
+                        for item in delivery_note.items:
+                            if item.against_sales_order == sales_order.name:
+                                qty_delivery = sales_order.custom_số_lượng_đã_giao - item.qty if sales_order.custom_số_lượng_đã_giao - item.qty > 0 else 0
+                                sales_order.custom_số_lượng_đã_giao = qty_delivery
+                                frappe.db.set_value("Sales Order", sales_order.name, "custom_số_lượng_đã_giao",
+                                                    qty_delivery)
+                                if qty_delivery > 0 and qty_delivery < sales_order.total_qty:
+                                    frappe.db.set_value("Sales Order", sales_order.name, "custom_trạng_thái_giao_hàng",
+                                                        "Đã giao một phần")
+                                elif qty_delivery == 0:
+                                    frappe.db.set_value("Sales Order", sales_order.name, "custom_trạng_thái_giao_hàng",
+                                                        "Đang giao hàng")
+                d.custom_updated_on_so = 0
+                frappe.db.set_value("Delivery Stop", d.name, "custom_updated_on_so", 0)
