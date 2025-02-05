04c8401f6100e39446465706993482f3a9ae959e
trinhpham1605 <trinhpham1605@gmail.com>
2025-02-05 14:16:37 +0700

    fix search product promotion

--- mbw_dms/api/selling/product.py
+++ mbw_dms/api/selling/product.py
@@ -18,7 +18,7 @@ def list_product(**kwargs):
 
         my_filter = {}
         name = kwargs.get("name")
-        name_item = kwargs.get("item_name")
+        key_search = kwargs.get("key_search")
         customer = kwargs.get("customer")
         brand = kwargs.get("brand")
         custom_industry = kwargs.get("industry")
@@ -39,11 +39,12 @@ def list_product(**kwargs):
                 price_lisr_cg = frappe.get_value("Customer Group", {"name": customers.customer_group}, "default_price_list")
                 if price_lisr_cg:
                     price_list = price_lisr_cg
-
+        filter_or = {}
         if name:
             my_filter["name"] = ["like", f'%{name}%']
-        if name_item:
-            my_filter["item_name"] = ["like", f'%{name_item}%']
+        if key_search:
+            filter_or["item_name"] = ["like", f'%{key_search}%']
+            filter_or["item_code"] = ["like", f'%{key_search}%']
         if brand:
             my_filter["brand"] = ["like", f'%{brand}%']
         if custom_industry:
@@ -52,13 +53,13 @@ def list_product(**kwargs):
             my_filter["item_group"] = ["like", f'%{item_group}%']
         my_filter["disabled"] = 0
 
-        items = frappe.db.get_list("Item", filters=my_filter,
-                                    fields=["name", "item_code", "item_name", "item_group", 
-                                            "stock_uom", "min_order_qty", "description",
-                                            "brand", "country_of_origin", "image",
-                                            "custom_industry", "end_of_life"],
-                                    start=page_size * (page_number - 1),
-                                    page_length=page_size)
+        items = frappe.db.get_list("Item", filters=my_filter, or_filters=filter_or,
+                                   fields=["name", "item_code", "item_name", "item_group",
+                                           "stock_uom", "min_order_qty", "description",
+                                           "brand", "country_of_origin", "image",
+                                           "custom_industry", "end_of_life"],
+                                   start=page_size * (page_number - 1),
+                                   page_length=page_size)
         for item in items:
             item_doc = frappe.get_doc("Item", item.get("name"))
             barcodes = item_doc.barcodes
@@ -66,7 +67,7 @@ def list_product(**kwargs):
 
             def return_fiel(value):
                 return pydash.pick(value, "link_image")
-            
+
             images_links = pydash.map_(images, return_fiel)
             item["custom_images_item"] = images_links
             item["barcodes"] = barcodes[0].barcode if barcodes else ""
@@ -85,7 +86,7 @@ def list_product(**kwargs):
         user_permissions = frappe.defaults.get_user_permissions(user)
         permitted_item_groups = set()
         permitted_item = set()
-        
+
         if "Item Group" in user_permissions or "Item" in user_permissions:
             if "Item Group" in user_permissions:
                 permitted_item_groups = {perm.get("doc") for perm in user_permissions.get("Item Group")}
@@ -100,7 +101,7 @@ def list_product(**kwargs):
                 if item.get("name") in permitted_item:
                     permitted_items_count += 1
         else:
-            permitted_items_count = frappe.db.count("Item", filters=my_filter)
+            permitted_items_count = len(frappe.db.get_list("Item", filters=my_filter, or_filters=filter_or, ))
 
         data_item = []
         default_selling_price_list = frappe.get_doc("Selling Settings").selling_price_list
@@ -329,6 +330,6 @@ def get_bin_item(warehouse, item_code):
     if item_code:
         query = query.where(bin.item_code == item_code)
     bin_list = query.run(as_dict=True)
-    print('========================= value: ', bin_list, flush=True)
+    # print('========================= value: ', bin_list, flush=True)
 
     return bin_list
